ccccde94a38396822d7c4dd50d24e882aad04478
hyeiin <86222656+itshyeiin@users.noreply.github.com>
2024-06-07 06:44:24 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,3 +99,24 @@ const scrollActive = () =>{
 	})
 }
 window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATIONS ===============*/
+const sr = ScrollReveal({
+   origin: 'top',
+   distance: '60px',
+   duration: 2500,
+   delay: 300,
+   // reset: true
+})
+
+sr.reveal('.home__data')
+sr.reveal('.home__circle, .home__img', {delay: 600, scale: 0.5})
+sr.reveal('.home__chips-1, .home__chips-2, .home__chips-3', {delay: 1000, interval: 600})
+sr.reveal('.home__leaf', {delay: 1200})
+sr.reveal('.home__tomato-1, .home__tomato-2', {delay: 1400, interval: 100})
+
+sr.reveal('.about__data', {delay: 800})
+sr.reveal('.about__img', {delay: 1200})
+
+sr.reveal('.products', {delay: 800})
+
